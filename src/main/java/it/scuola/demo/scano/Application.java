fa0a2a4b02b18d5fbe00e957b283d265fda4c213
sandroscano <scano.sandro@iisbuonarrotivolta.it>
2026-04-09 11:17:20 +0200

    Add files via upload

---
+++ src/main/java/it/scuola/demo/scano/Application.java
@@ -0,0 +1,13 @@
+package it.scuola.demo.scano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
